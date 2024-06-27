2b3019e59c1d892970cc39a181ed19218cb78b4c
TinyTalls <wyattrnewell@gmail.com>
2024-06-27 14:43:55 -0400

    wrote function Main

--- 1_HangmanGame.py
+++ 1_HangmanGame.py
@@ -14,3 +14,19 @@ print(f"Hello, {player_name}! Good luck, and don't get hanged!")
 time.sleep(2)
 print(f"The game is about to begin!")
 time.sleep(3)
+
+def main():
+    global count
+    global display
+    global already_guessed 
+    global length
+    global play_game
+    words_to_guess = ["abundance", "benevolent", "contradict", "diligent", "eloquent", "fortitude", 
+                      "gracious", "hinder", "illustrate", "jovial", "knowledgeable", 
+                      "meticulous", "nostalgia", "perservere", "resilient"]
+    word = random.choice(words_to_guess)
+    length = len(word)
+    count = 0
+    display = '_' * length
+    already_guessed = []
+    play_game = ""
