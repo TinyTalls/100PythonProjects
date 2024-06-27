cfb9db61982c96ecb514d3d09ddb61e6476a7de7
TinyTalls <wyattrnewell@gmail.com>
2024-06-27 14:35:14 -0400

    Initialize Game

---
+++ 1_HangmanGame.py
@@ -0,0 +1,16 @@
+"""
+Hangman Game
+100 Python Projects
+by Wyatt Newell
+6/27/2024
+"""
+
+import random, time
+
+# Initialize Game
+print("\nHangman by Wyatt Newell\n")
+player_name = input(f"Please enter your name: ")
+print(f"Hello, {player_name}! Good luck, and don't get hanged!")
+time.sleep(2)
+print(f"The game is about to begin!")
+time.sleep(3)
