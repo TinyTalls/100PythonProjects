9445891b3344d2ea5260815a438526cb884549f4
TinyTalls <wyattrnewell@gmail.com>
2024-06-28 13:40:16 -0400

    Polishing

--- 2_TipCalculator.py
+++ 2_TipCalculator.py
@@ -5,11 +5,11 @@ by Wyatt Newell
 """
 import time
 
-# print(f"\nWelcome to Tip Calculator by Wyatt Newell\n")
-# time.sleep(2)
-# user_name = input(f"Please enter your name: ")
-# print(f"Hello, {user_name}! The program is about to begin.\n")
-# time.sleep(2)
+print(f"\nWelcome to Tip Calculator by Wyatt Newell\n")
+time.sleep(1)
+user_name = input(f"Please enter your name: ")
+print(f"Hello, {user_name}! The program is about to begin.\n")
+time.sleep(1)
 
 def main():
     # Find the initial amount from the user
@@ -20,7 +20,7 @@ def main():
         print("Invalid value, please try again")
         main()
     
-    # Determine Tip Percentage
+    # Input tip percentage
     print(f"Select Tip Percentage or select Custom")
     print(f"1) 10%\n2) 15%\n3) 20%\n4) 22%\n5) Custom\n")
     tip_selection = input(f"Enter Selection: ")
@@ -28,17 +28,22 @@ def main():
         print("\nInvalid Selection")
         tip_selection = input("Enter Selection: ")
 
+    # Calculate tip and sum for total amount
     tip_percentages = {"1": 0.10, "2": 0.15, "3": 0.20, "4": 0.22}
 
     if tip_selection in tip_percentages:
         tipped_amount = float(amount) * tip_percentages[tip_selection]
         total_amount = float(amount) + tipped_amount
-        print(f"Your {int(tip_percentages[tip_selection] * 100)}% tip for ${amount} is ${tipped_amount:.2f} for a total of ${total_amount:.2f}")
+        time.sleep(1)
+        print(f"\n{user_name}, your {int(tip_percentages[tip_selection] * 100)}% tip for ${amount} is ${tipped_amount:.2f} for a total of ${total_amount:.2f}")
+
+    # Custom tip percent input     
     elif tip_selection == "5":
         custom_tip_percentage = float(input("Insert your custom tip %: ")) / 100
         tipped_amount = float(amount) * custom_tip_percentage
         total_amount = float(amount) + tipped_amount
-        print(f"Your custom tip for ${amount} is ${tipped_amount:.2f} for a total of ${total_amount:.2f}")
+        time.sleep(1)
+        print(f"\n{user_name}, your custom tip for ${amount} is ${tipped_amount:.2f} for a total of ${total_amount:.2f}")
 
     
     
