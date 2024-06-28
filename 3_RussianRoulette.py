fa87e8c85700c1acc5b7b5c86f6d72d279b1f0e0
TinyTalls <wyattrnewell@gmail.com>
2024-06-28 16:10:49 -0400

    coin_flip_game Trimmed

--- 3_RussianRoulette.py
+++ 3_RussianRoulette.py
@@ -16,28 +16,33 @@ The player and computer may only spin the barrel once per game.
 import time, random
 
 # Initialize Game
-print("\nRussian Roulette by Wyatt Newell\n")
-player_name = input(f"Please enter your name: ")
-print(f"Hello, {player_name}! Good luck, and don't get shot!")
-time.sleep(2)
-print(f"The game is about to begin!")
-input(f"Press Enter to Start")
-time.sleep(3)
+# print("\nRussian Roulette by Wyatt Newell\n")
+# player_name = input(f"Please enter your name: ")
+# print(f"Hello, {player_name}! Good luck, and don't get shot!")
+# time.sleep(2)
+# print(f"The game is about to begin!")
+# input(f"Press Enter to Start")
+# time.sleep(3)
 
+# Coin flip to determine who goes first
 def coin_flip_game():
     print(f"\nYou find yourself in a dark room sitting at a small wooden table. There's a revolver and a bullet."
             "\nFrom the darkness a robotic man leans his monitor face forward and asks with a pixel grin."
             "\n\n'Heads or Tails?'")
     time.sleep(1)
-    print(f"1) Heads"
-            "\n2) Tails\n")
-    coin_flip_choice = input(f"Enter Selection: ")
-    while coin_flip_choice not in ["1", "2"]:
-        print(f"'ERROR' the computer monitor reads, 'Heads or Tails.' its speaker moans.")
-        time.sleep(1)
+
+    # Player coin flip selection
+    while True:
         print(f"1) Heads"
-            "\n2) Tails\n")
+                "\n2) Tails\n")
         coin_flip_choice = input(f"Enter Selection: ")
+        if coin_flip_choice in ["1", "2"]:
+            break
+        else:
+            print(f"'ERROR' the computer monitor reads, 'Heads or Tails.' its speaker moans.")
+            time.sleep(1)
+
+    # The Flip
     print(f"A pixel hand holding a coin appears on the monitor. It flicks the coin with its thumb, flipping in virtual air.")
     coin_flip = random.randint(1,2)
     time.sleep(1)
@@ -45,25 +50,24 @@ def coin_flip_game():
     time.sleep(1)
     print(f"The pixel hand catches the coin! Another hand appears, and the coin is flipped into it.")
     time.sleep(1)
-    print(f"The palm opens...")
-    if coin_flip == 1:
-        print(f"Heads!")
-    elif coin_flip == 2:
-        print(f"Tails!")
-    if coin_flip == 1 and coin_flip_choice == "1" or coin_flip == 2 and coin_flip_choice == "2":
-        print(f"You win!")
-        return True
-    elif coin_flip == 1 and coin_flip_choice == "2" or coin_flip == 2 and coin_flip_choice == "1":
-        print(f"You lose!")
-        return False 
+    print(f"The palm opens...\n")
+    time.sleep(1)
 
+    print(f"Heads!" if coin_flip == 1 else f"Tails!")
+    if (coin_flip == 1 and coin_flip_choice == "1") or (coin_flip == 2 and coin_flip_choice == "2"):
+        print(f"\nYou win!")
+        return True
+    else:
+        print(f"\nYou lose!")
+        return False 
 
 
 # Main Game
 def main():
-    if coin_flip_game() == True:
+    coin_flip_result = coin_flip_game()
+    if coin_flip_result == True:
         print(f"Player Goes First")
-    elif coin_flip_game() == False:
+    elif coin_flip_result == False:
         print(f"Computer Goes First")
     
 main()
