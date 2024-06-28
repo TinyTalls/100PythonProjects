e43db789989c7a806772575e69137a15869a925f
TinyTalls <wyattrnewell@gmail.com>
2024-06-28 12:59:19 -0400

    2_TipCalculator Start

---
+++ 2_TipCalculator.py
@@ -0,0 +1,13 @@
+"""
+Tip Calculator
+by Wyatt Newell
+6/28/2024
+"""
+import time
+
+print(f"\nWelcome to Tip Calculator by Wyatt Newell\n")
+time.sleep(2)
+player_name = input(f"Please enter your name: ")
+print(f"Hello, {player_name}! The program is about to begin.")
+time.sleep(2)
+
