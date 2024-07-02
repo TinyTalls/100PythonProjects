90051bbdc7ba3e5f660b0dbe7c584ec49ab4a3ca
TinyTalls <wyattrnewell@gmail.com>
2024-07-02 14:53:00 -0400

    PIL test

--- 4_ImageResizer/4_ImageResizer.py
+++ 4_ImageResizer/4_ImageResizer.py
@@ -9,5 +9,20 @@ I have not worked with image manipulation in python before, so this will offer a
 There are packages already available for such a project. This will help, however my end goal is for the user to have a simple GUI that will allow for input of an image,
 resize options, and output path for said image. I would prefer the user not having to write within the python code itself. 
 """
+from PIL import Image
+import os
 
+image_path = "c:/Users/tinyt/OneDrive/Desktop/100PythonProjects/4_ImageResizer/Checkmark.jpg"
 
+# # PIL Test
+if os.path.isfile(image_path):
+    print("File Exists.")
+else:
+    print("File does not exist.")
+
+try:
+    img = Image.open(image_path)
+    img.show()
+    print("Image loaded successfully with PIL.")
+except Exception as e:
+    print(f"Error: {e}")
