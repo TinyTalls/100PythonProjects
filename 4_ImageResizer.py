cb48d791371c4e543741013ede98a7818ce27b00
TinyTalls <wyattrnewell@gmail.com>
2024-07-02 14:08:09 -0400

    4_ImageResizer.py start

---
+++ 4_ImageResizer.py
@@ -0,0 +1,13 @@
+"""
+Image Resizer
+by Wyatt Newell
+7/2/2024
+
+The following program is an image resizer. I had a recent problem which required such a program, and I wasn't all too thrilled with the options I had. 
+I have not worked with image manipulation in python before, so this will offer a good learning opportunity. 
+
+There are packages already available for such a project. This will help, however my end goal is for the user to have a simple GUI that will allow for input of an image,
+resize options, and output path for said image. I would prefer the user not having to write within the python code itself. 
+"""
+
+
