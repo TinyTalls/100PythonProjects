1e221ac8fb061afeb89be5b1ea3c90c3d533ffcc
TinyTalls <wyattrnewell@gmail.com>
2024-08-09 14:21:35 -0400

    initial main_menu function created

--- 5_Wy-Park/wy-park.py
+++ 5_Wy-Park/wy-park.py
@@ -25,7 +25,46 @@ def print_current_time():
 
 
 def intro():
-    print("\n" + "+" * 50)
+    """
+    intro = prints the Wy-Park intro
+    """
+    print("\n" + ("+" * 50) + "\n")
     print(" Wy-Park ".center(50))
-    print(" The Future of Parking ".center(50) + "\n" + "+" * 50 + "\n")
-intro()
+    print(" The Future of Parking ".center(50) + "\n\n" + "+" * 50 + "\n")
+
+def main_menu():
+    intro()
+    while True:
+        print("Current Time".center(50))
+        print(date.today().strftime("%B %d, %Y").center(50))
+        print(datetime.now().strftime("%I:%M:%S %p").center(50) + "\n")
+
+        menu_options = [
+            "1. Pay for Parking",
+            "2. Extend Parking",
+            "3. Check Time",
+            "4. Parking Control",
+            "5. Exit"
+        ]
+
+        print("\n".join(menu_options))
+        user_selection = input("\nEnter Selection: ")
+        time.sleep(2)
+
+        if user_selection not in ["1", "2", "3", "4", "5"]:
+            print(f"\nError: Please input valid selection.\n")
+            time.sleep(2)
+        elif user_selection == "1":
+            print("Pay for Parking... Coming Soon!")
+        elif user_selection == "2":
+            print("Extend Parking... Coming Soon!")
+        elif user_selection == "3":
+            print("Check Time... Coming Soon!")
+        elif user_selection == "4":
+            print("Parking Control... Coming Soon!")
+        elif user_selection == "5":
+            break
+
+
+
+main_menu()
